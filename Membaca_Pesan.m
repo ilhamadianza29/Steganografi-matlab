342c0eb80947cb454c113df0c62d1f2bd95c73ca
yahyawildan <yahyawildan.id@gmail.com>
2021-07-02 21:12:06 +0700

    Menghapus file yang tidak dibutuhkan

--- Membaca_Pesan.m
+++
@@ -1,9 +0,0 @@
-%% Impor gambar dengan gambar tersembunyi
-
-Im = imread('MsgIm.bmp');
-
-%% Ekstrak bitplane dari Message Signal
-PesanGambar = bitget(Im(:,:,1),1);
-
-%% Visualisasikan Pesannya
-imshow(logical(PesanGambar));
